e992762c585049a3d129c2203bcd58c242942915
Shrit Shrivastava <shrit1401@gmail.com>
2025-05-07 22:47:46 +0530

    feat: add metadata generation for chapter pages

--- src/app/comic/[title]/[chapter]/page.tsx
+++ src/app/comic/[title]/[chapter]/page.tsx
@@ -1,19 +1,33 @@
 import { getChapterPages } from "./actions";
 import ChapterView from "@/app/comic/[title]/[chapter]/ChapterView";
+import { Metadata } from "next";
 
-type Params = {
+type Params = Promise<{
   title: string;
   chapter: string;
-};
+}>;
 
 interface Props {
   params: Params;
   searchParams: { [key: string]: string | string[] | undefined };
 }
 
+export async function generateMetadata({
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const { title, chapter } = await params;
+
+  return {
+    title: `${title} - Chapter ${chapter}`,
+    description: `Read ${title} Chapter ${chapter}`,
+  };
+}
+
 export default async function Page(props: Props) {
-  const { title, chapter } = props.params;
+  const { title, chapter } = await props.params;
   const pages = await getChapterPages(title, chapter);
 
-  return <ChapterView params={props.params} pages={pages} />;
+  return <ChapterView params={{ title, chapter }} pages={pages} />;
 }
